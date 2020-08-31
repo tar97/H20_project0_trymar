fef06057c80d94ec121bb2ddf78c08bb8b2e8403
tar <tar>
2020-08-31 16:40:29 +0200

    Add test for floating point addition

--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,11 @@ import calculator
 def test_add_exercise_1():
     a, b = 1, 2
     assert calculator.add(a, b) == 3
+
+
+def test_add_fl_exercise_2():
+    a, b = 0.1, 0.2
+    ans = 0.3
+    eps = 1e-12
+    assert abs(calculator.add(a, b) - ans)\
+           < eps
