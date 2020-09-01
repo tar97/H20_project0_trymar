8a45b81d27b3d203debf9919f108a0f364c2697a
trymar <trymar@mail.uio.no>
2020-09-01 13:36:47 +0200

    adds factorial, divide, sin, signun and binomial functions and tests

--- calculator.py
+++ calculator.py
@@ -1,3 +1,39 @@
 def add(x, y):
     return x + y
 
+
+def factorial(n):
+    k = n
+    res = 1
+    while k > 0:
+        res *= k
+        k -= 1
+    return res
+
+
+def sin(x, n):
+    res = 0
+    for i in range(n+1):
+        num = (-1)**i * x**(2*i + 1)
+        denom = factorial(2*i + 1)
+        res += num / denom
+    return res
+
+
+def divide(x, y):
+    return float(x) / y
+
+
+def binomial(n, k):
+    num = factorial(n)
+    denom = factorial(k) * factorial(n - k)
+    return num / denom
+
+
+def signum(x):
+    if x < 0:
+        return -1
+    elif x == 0:
+        return 0
+    else:
+        return 1
--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,6 @@
 import calculator
+import math
+eps = 1e-12
 
 
 def test_add_exercise_1():
@@ -9,7 +11,6 @@ def test_add_exercise_1():
 def test_add_fl_exercise_2():
     a, b = 0.1, 0.2
     ans = 0.3
-    eps = 1e-12
     assert abs(calculator.add(a, b) - ans)\
            < eps
 
@@ -19,3 +20,113 @@ def test_add_str_exercise_3():
     sentence = "Hello World"
     assert calculator.add(w1, w2) \
         == sentence
+
+
+def test_exponential_exercise_4():
+    assert calculator.factorial(0) == 1
+    x = 10
+    ans = 3628800
+    assert calculator.factorial(x) \
+           == ans
+
+
+def test_sin_zero_exercise_4():
+    acc = 50
+    x = 0
+    ans = 0
+    diff = abs(calculator.sin(x, acc) - ans)
+    assert diff < eps
+
+
+def test_sin_fourth_exercise_4():
+    acc = 50
+    x = math.pi/4
+    ans = math.pow(2, 0.5) / 2
+    diff = abs(calculator.sin(x, acc) - ans)
+    assert diff < eps
+
+
+def test_sin_half_exercise_4():
+    acc = 50
+    x = math.pi / 2
+    ans = 1
+    diff = abs(calculator.sin(x, acc) - ans)
+    assert diff < eps
+
+
+def test_sin_2quad_exercise_4():
+    acc = 50
+    x = 5 * math.pi / 6
+    ans = 1. / 2
+    diff = abs(calculator.sin(x, acc) - ans)
+    assert diff < eps
+
+
+def test_sin_3quad_exercise_4():
+    acc = 50
+    x = 7 * math.pi / 6
+    ans = - 1.0 / 2
+    diff = abs(calculator.sin(x, acc) - ans)
+    assert diff < eps
+
+
+def test_sin_4quad_exercise_4():
+    acc = 50
+    x = 7 * math.pi / 4
+    ans = - math.pow(2, 0.5) / 2
+    diff = abs(calculator.sin(x, acc) - ans)
+    assert diff < eps
+
+
+def test_divide_zero_exercise_4():
+    x, y = 0, 5
+    assert calculator.divide(x, y) \
+        == 0
+
+
+def test_divide_one_exercise_4():
+    x, y = 10, 1
+    assert calculator.divide(x, y) \
+        == x
+
+
+def test_divide_exercise_4():
+    x, y = 15, 5
+    assert calculator.divide(x, y) \
+        == 3
+
+
+def test_divide_frac_exercise_4():
+    x, y = 5, 10
+    assert abs(calculator.divide(x, y) - 0.5) \
+        < eps
+
+
+def test_binomial_zero_exercise_4():
+    n, k = 10, 0
+    assert abs(calculator.binomial(n, k) - 1) \
+        < eps
+
+
+def test_binomial_exercise_4():
+    n, k = 10, 6
+    ans = 210
+    assert abs(calculator.binomial(n, k) - ans) \
+        < eps
+
+
+def test_signum_neg_exercise_4():
+    x = -5
+    assert calculator.signum(x) == -1
+
+
+def test_signum_zero_exercise_4():
+    x = 0
+    assert calculator.signum(x) == 0
+
+
+def test_signum_pos_exercise_4():
+    x = 6
+    assert calculator.signum(x) == 1
+
+
