32a5bbbd05ea1182901516fd3545fde62f7ddd65
trymar <trymar@mail.uio.no>
2020-09-01 14:38:24 +0200

    parameterized tests

--- test_calculator.py
+++ test_calculator.py
@@ -4,108 +4,70 @@ import pytest
 eps = 1e-12
 
 
-def test_add_exercise_1():
-    a, b = 1, 2
-    assert calculator.add(a, b) == 3
+@pytest.mark.parametrize("arg, exp", [[(1, 2), 3],
+                                      [(5, 6), 11],
+                                      [(0, 1), 1],
+                                      [(-4, 10), 6]])
+def test_add_exercise_1(arg, exp):
+    assert calculator.add(arg[0], arg[1]) == exp
 
 
-def test_add_fl_exercise_2():
-    a, b = 0.1, 0.2
-    ans = 0.3
-    assert abs(calculator.add(a, b) - ans)\
+@pytest.mark.parametrize("arg, exp", [[(1.0, 2.0), 3.0],
+                                      [(5.0, 6.0), 11.0],
+                                      [(0.0, 1.0), 1.0],
+                                      [(-4.0, 10.0), 6.0],
+                                      [(0.5, 0.4), 0.9]])
+def test_add_fl_exercise_2(arg, exp):
+    a, b = arg
+    assert abs(calculator.add(a, b) - exp)\
            < eps
 
 
-def test_add_str_exercise_3():
-    w1, w2 = "Hello ", "World"
-    sentence = "Hello World"
+@pytest.mark.parametrize("arg, exp", [[("Hello ", "world"), "Hello world"],
+                                      [("", "h"), "h"],
+                                      [("hey", " "), "hey "]])
+def test_add_str_exercise_3(arg, exp):
+    w1, w2 = arg
     assert calculator.add(w1, w2) \
-        == sentence
+        == exp
 
 
-def test_add_typerr_exercise_5():
+@pytest.mark.parametrize("arg", [("Hello ", 5),
+                                 ("", -8)])
+def test_add_typerr_exercise_5(arg):
     with pytest.raises(TypeError):
-        calculator.add("hello", 5)
+        calculator.add(arg)
 
 
-def test_exponential_exercise_4():
-    assert calculator.factorial(0) == 1
-    x = 10
-    ans = 3628800
-    assert calculator.factorial(x) \
-           == ans
+@pytest.mark.parametrize("arg, exp", [(0, 1),
+                                      (10, 3628800),
+                                      (4, 24)])
+def test_exponential_exercise_4(arg, exp):
+    assert calculator.factorial(arg) \
+           == exp
 
 
-def test_sin_zero_exercise_4():
+@pytest.mark.parametrize("arg, exp", [(0, 0),
+                                      (math.pi/4, math.pow(2, 0.5) / 2),
+                                      (math.pi/2, 1),
+                                      (5 * math.pi / 6, 1.0 / 2),
+                                      (7 * math.pi / 6, -1.0 / 2),
+                                      (7 * math.pi / 4, - math.pow(2, 0.5) / 2),
+                                      (2*math.pi, 0)])
+def test_sin_exercise_5(arg, exp):
     acc = 50
-    x = 0
-    ans = 0
-    diff = abs(calculator.sin(x, acc) - ans)
+    diff = abs(calculator.sin(arg, acc) - exp)
     assert diff < eps
 
 
-def test_sin_fourth_exercise_4():
-    acc = 50
-    x = math.pi/4
-    ans = math.pow(2, 0.5) / 2
-    diff = abs(calculator.sin(x, acc) - ans)
-    assert diff < eps
-
-
-def test_sin_half_exercise_4():
-    acc = 50
-    x = math.pi / 2
-    ans = 1
-    diff = abs(calculator.sin(x, acc) - ans)
-    assert diff < eps
-
-
-def test_sin_2quad_exercise_4():
-    acc = 50
-    x = 5 * math.pi / 6
-    ans = 1. / 2
-    diff = abs(calculator.sin(x, acc) - ans)
-    assert diff < eps
-
-
-def test_sin_3quad_exercise_4():
-    acc = 50
-    x = 7 * math.pi / 6
-    ans = - 1.0 / 2
-    diff = abs(calculator.sin(x, acc) - ans)
-    assert diff < eps
-
-
-def test_sin_4quad_exercise_4():
-    acc = 50
-    x = 7 * math.pi / 4
-    ans = - math.pow(2, 0.5) / 2
-    diff = abs(calculator.sin(x, acc) - ans)
-    assert diff < eps
-
-
-def test_divide_zero_exercise_4():
-    x, y = 0, 5
+@pytest.mark.parametrize("args, exp", [[(0, 5), 0],
+                                        [(10, 1), 10],
+                                       [(15, 5), 3],
+                                       [(5, 10), 0.5]])
+def test_divide_zero_exercise_5(args, exp):
+    x, y = args
     assert calculator.divide(x, y) \
-        == 0
-
-
-def test_divide_one_exercise_4():
-    x, y = 10, 1
-    assert calculator.divide(x, y) \
-        == x
-
-
-def test_divide_exercise_4():
-    x, y = 15, 5
-    assert calculator.divide(x, y) \
-        == 3
-
-
-def test_divide_frac_exercise_4():
-    x, y = 5, 10
-    assert abs(calculator.divide(x, y) - 0.5) \
-        < eps
+        == exp
 
 
 def test_divide_zerodiverr_exercise_5():
@@ -113,31 +75,16 @@ def test_divide_zerodiverr_exercise_5():
         calculator.divide(5, 0)
 
 
-def test_binomial_zero_exercise_4():
-    n, k = 10, 0
-    assert abs(calculator.binomial(n, k) - 1) \
+@pytest.mark.parametrize("args, exp", [[(10, 0), 1],
+                                       [(10, 6), 210]])
+def test_binomial_exercise_5(args, exp):
+    n, k = args
+    assert abs(calculator.binomial(n, k) - exp) \
         < eps
 
 
-def test_binomial_exercise_4():
-    n, k = 10, 6
-    ans = 210
-    assert abs(calculator.binomial(n, k) - ans) \
-        < eps
-
-
-def test_signum_neg_exercise_4():
-    x = -5
-    assert calculator.signum(x) == -1
-
-
-def test_signum_zero_exercise_4():
-    x = 0
-    assert calculator.signum(x) == 0
-
-
-def test_signum_pos_exercise_4():
-    x = 6
-    assert calculator.signum(x) == 1
-
-
+@pytest.mark.parametrize("arg, exp", [(-5, -1),
+                                      (0, 0),
+                                      (5, 1)])
+def test_signum_exercise_5(arg, exp):
+    assert calculator.signum(arg) == exp
